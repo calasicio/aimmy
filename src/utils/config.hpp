edcf3e0eca492784c19ab8e51dc5dc597aecd7ea
Calasicio <calasicio@gmail.com>
2026-08-25 14:17:59 +0700

    feat: tune default values

--- src/utils/config.hpp
+++ src/utils/config.hpp
@@ -5,9 +5,9 @@ namespace Config
 {
   inline bool rcsEnabled = true;
   inline bool rcsHumanizerEnabled = true;
-  inline int rcsPerfection = 50;
-  inline int rcsSmoothness = 50;
-  inline int rcsJitter = 50;
+  inline int rcsPerfection = 70;
+  inline int rcsSmoothness = 100;
+  inline int rcsJitter = 100;
 
   void LoadSettings(std::string_view filePath);
 }
