ddffeb0d8e0d3c0a5bafdb3201d67440f7c5b35c
Calasicio <calasicio@gmail.com>
2026-08-29 18:43:17 +0700

    feat: add keybind to stop the program

--- src/core/cheat/cheat.cpp
+++ src/core/cheat/cheat.cpp
@@ -1,5 +1,8 @@
 #include "cheat.hpp"
 
+#include <chrono>
+#include <thread>
+
 #include "core/engine/engine.hpp"
 #include "utils/logger/logger.hpp"
 
@@ -30,6 +33,15 @@ void Cheat::threadImpl()
 {
   while (isRunning)
   {
+    auto start = std::chrono::steady_clock::now();
+
+    if (GetAsyncKeyState(VK_END) & 0x8000)
+    {
+      isRunning = false;
+      break;
+    }
+
+    std::this_thread::sleep_until(start + std::chrono::milliseconds(1));
   }
 }
 
