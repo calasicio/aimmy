5401e7febfb3c9b92fafa78ad373bf80e12f2ca4
Calasicio <calasicio@gmail.com>
2026-08-29 18:00:14 +0700

    feat: make engine become easier to access

--- src/core/engine/engine.cpp
+++ src/core/engine/engine.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <thread>
 
+using namespace std::chrono_literals;
+
 #include "core/offsets/dumper.hpp"
 #include "utils/logger/logger.hpp"
 
@@ -11,16 +13,34 @@ bool Engine::init()
   return getInstance().initImpl();
 }
 
-pProcess &Engine::getProcess()
+ProcessModule Engine::getClient()
 {
-  return getInstance().process_;
+  return getInstance().client;
+}
+
+ProcessModule Engine::getEngine()
+{
+  return getInstance().engine;
+}
+
+std::shared_ptr<pProcess> Engine::getProcess()
+{
+  return getInstance().process;
 }
 
 bool Engine::initImpl()
 {
-  if (!process_.AttachProcess("cs2.exe"))
+  process = std::make_shared<pProcess>();
+
+  if (!this->awaitProcess())
   {
     logger::fatal("Could not find process, please make sure the game is open");
+    return false;
+  }
+
+  if (!this->awaitModules())
+  {
+    logger::fatal("Game took too long to load, please open me again once its fully loaded");
     return false;
   }
 
@@ -35,6 +55,65 @@ bool Engine::initImpl()
   }
 
   logger::info("Successfully initialized engine...");
+
+  return true;
+}
+
+bool Engine::awaitProcess()
+{
+  if (!process || process->handle_)
+  {
+    return false;
+  }
+
+  do
+  {
+    if (process->AttachProcess("cs2.exe"))
+      break;
+
+    if (process->pid_ && !process->handle_)
+    {
+      logger::fatal("Insufficient permissions to open a handle to the process. Try running as Administrator.");
+      return false;
+    }
+
+    static int attempts = 0;
+
+    if (!attempts)
+    {
+      logger::info("Waiting 50s for the game to open..");
+    }
+
+    if (attempts > 10)
+      return false;
+    attempts++;
+
+    std::this_thread::sleep_for(5s);
+  } while (true);
+}
+
+bool Engine::awaitModules()
+{
+  if (!process || !process->handle_)
+    return false;
+
+  logger::info("Waiting for the game to load..");
+
+  do
+  {
+    this->client = process->GetModule("client.dll");
+    this->engine = process->GetModule("engine2.dll");
+
+    if (this->client.base && this->engine.base)
+      break;
+
+    static int attempts = 0;
+    if (attempts > 10)
+      return false;
+    attempts++;
+
+    std::this_thread::sleep_for(5s);
+  } while (true);
 
   return true;
 }
--- src/core/engine/engine.hpp
+++ src/core/engine/engine.hpp
@@ -7,19 +7,34 @@
 class Engine
 {
 public:
-  static bool init();
-  static pProcess &getProcess();
+  ~Engine() = default;
+  Engine(const Engine &) = delete;
+  Engine(Engine &&) = delete;
+  Engine &operator=(const Engine &) = delete;
+  Engine &operator=(Engine &&) = delete;
 
+  static bool init();
+  static ProcessModule getClient();
+  static ProcessModule getEngine();
+  static std::shared_ptr<pProcess> getProcess(); // Refactor this so its easier to access
 private:
-  Engine() = default;
+  Engine() {};
 
   static Engine &getInstance()
   {
-    static Engine instance;
-    return instance;
+    static Engine i{};
+    return i;
   }
 
   bool initImpl();
 
-  pProcess process_;
+  bool awaitProcess();
+  bool awaitModules();
+
+  void thread();
+
+private:
+  std::shared_ptr<pProcess> process;
+  ProcessModule client;
+  ProcessModule engine;
 };
