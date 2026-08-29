cf0e519a52a3aa824e85d3b0afb064134b214e79
Calasicio <calasicio@gmail.com>
2026-08-29 23:57:47 +0700

    fix(rcs): fix rcs jumping around when bursting
    Fixed a problem where the aim is flicking all weird when bursting. This is caused by incorrectly setting the old aim punch state to 0 when the firing is stopped causing a massive delta spike,.

--- src/features/rcs/rcs.cpp
+++ src/features/rcs/rcs.cpp
@@ -7,6 +7,7 @@
 #include "utils/logger/logger.hpp"
 
 const float CORRECTION_FACTOR = 3.37;
+const float YAW_PITCH_FACTOR = 0.022;
 
 void RCS::update(float dt)
 {
@@ -15,25 +16,27 @@ void RCS::update(float dt)
   if (!snapshot.localPlayer.isAlive)
   {
     oldAimPunch = {0.0, 0.0};
-    return;
-  }
-
-  if (snapshot.localPlayer.shotsFired <= 1)
-  {
-    oldAimPunch = {0.0f, 0.0f};
+    accumulatedError = {0.0, 0.0};
     return;
   }
 
   Vector3 aimPunch = snapshot.localPlayer.aimPunch;
   Vector2 currentAimPunch = {aimPunch.x, aimPunch.y};
 
+  if (snapshot.localPlayer.shotsFired <= 1)
+  {
+    oldAimPunch = currentAimPunch;
+    accumulatedError = {0.0, 0.0};
+    return;
+  }
+
   Vector2 deltaPunch = (currentAimPunch - oldAimPunch);
 
   deltaPunch *= -1.0 * CORRECTION_FACTOR;
 
   Vector2 moveAmount = {
-      (deltaPunch.y / snapshot.globals.sensitivity) / -0.022f + accumulatedError.x,
-      (deltaPunch.x / snapshot.globals.sensitivity) / 0.022f + accumulatedError.y};
+      (deltaPunch.y / snapshot.globals.sensitivity) / -YAW_PITCH_FACTOR + accumulatedError.x,
+      (deltaPunch.x / snapshot.globals.sensitivity) / YAW_PITCH_FACTOR + accumulatedError.y};
 
   int moveX = static_cast<int>(moveAmount.x);
   int moveY = static_cast<int>(moveAmount.y);
