a8109d80f2fd171d536aae4cb7465efa6b991c06
Calasicio <calasicio@gmail.com>
2026-08-29 23:25:22 +0700

    feat: use proper constants instead of magic values

--- src/features/rcs/rcs.cpp
+++ src/features/rcs/rcs.cpp
@@ -6,6 +6,8 @@
 #include "utils/mouse/mouse.hpp"
 #include "utils/logger/logger.hpp"
 
+const float CORRECTION_FACTOR = 3.37;
+
 void RCS::update(float dt)
 {
   auto snapshot = Cache::copySnapshot();
@@ -25,7 +27,7 @@ void RCS::update(float dt)
   Vector3 aimPunch = snapshot.localPlayer.aimPunch;
   Vector2 currentAimPunch = {aimPunch.x, aimPunch.y};
 
-  Vector2 deltaPunch = (currentAimPunch - oldAimPunch) * -1.0 * 3.37;
+  Vector2 deltaPunch = (currentAimPunch - oldAimPunch) * -1.0 * CORRECTION_FACTOR;
 
   Vector2 moveAmount = {
       (deltaPunch.y / snapshot.globals.sensitivity) / -0.022f,
@@ -33,72 +35,6 @@ void RCS::update(float dt)
 
   int moveXAmountInPixel = static_cast<int>(moveAmount.x);
   int moveYAmountInPixel = static_cast<int>(moveAmount.y);
-
-  logger::debug(
-      "aimPunch = { x: " +
-      std::to_string(aimPunch.x) +
-      ", y: " +
-      std::to_string(aimPunch.y) +
-      ", z: " +
-      std::to_string(aimPunch.z) +
-      " }");
-
-  logger::debug(
-      "currentAimPunch = { x: " +
-      std::to_string(currentAimPunch.x) +
-      ", y: " +
-      std::to_string(currentAimPunch.y) +
-      " }");
-
-  logger::debug(
-      "oldAimPunch = { x: " +
-      std::to_string(oldAimPunch.x) +
-      ", y: " +
-      std::to_string(oldAimPunch.y) +
-      " }");
-
-  logger::debug(
-      "deltaPunch = { x: " +
-      std::to_string(deltaPunch.x) +
-      ", y: " +
-      std::to_string(deltaPunch.y) +
-      " }");
-
-  logger::debug(
-      "sensitivity = " +
-      std::to_string(snapshot.globals.sensitivity));
-
-  logger::debug(
-      "moveAmount = { x: " +
-      std::to_string(moveAmount.x) +
-      ", y: " +
-      std::to_string(moveAmount.y) +
-      " }");
-
-  logger::debug(
-      "moveXAmountInPixel = " +
-      std::to_string(moveXAmountInPixel) +
-      ", moveYAmountInPixel = " +
-      std::to_string(moveYAmountInPixel));
-  logger::debug(
-      "RCS | punch(" +
-      std::to_string(aimPunch.x) + ", " +
-      std::to_string(aimPunch.y) +
-      ") old(" +
-      std::to_string(oldAimPunch.x) + ", " +
-      std::to_string(oldAimPunch.y) +
-      ") delta(" +
-      std::to_string(deltaPunch.x) + ", " +
-      std::to_string(deltaPunch.y) +
-      ") sens=" +
-      std::to_string(snapshot.globals.sensitivity) +
-      " move(" +
-      std::to_string(moveAmount.x) + ", " +
-      std::to_string(moveAmount.y) +
-      ") pixels(" +
-      std::to_string(moveXAmountInPixel) + ", " +
-      std::to_string(moveYAmountInPixel) +
-      ")");
 
   if (moveXAmountInPixel != 0 || moveYAmountInPixel != 0)
   {
